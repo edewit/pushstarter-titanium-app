f1174416f89250826bc5cbd583b27186a2ff0439
Erik Jan de Wit <erikjan.dewit@gmail.com>
2016-04-15 19:15:14 +0200

    use AeroGearPush for iOS

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,40 +1,36 @@
-var push = require('org.jboss.aerogear.push');
 var items = [];
 
-function onNotification(e) {
-	items.push({ label: {text: e.alert}});
-
-	$.section.setItems(items);
-	$.main.open();
-}
-
-function eventSuccess() {
-	$.empty.open();
-}
-
-function eventError(e) {
-	Ti.API.info('Error:' + e.error);
-	var alert = Ti.UI.createAlertDialog({
-		title : 'Error',
-		message : e.error
-	});
-	alert.show();
-}
-
-var pushConfig = {
+var AeroGearPush = require('AeroGearPush').init({
 	pushServerURL : "https://unifiedpush-edewit.rhcloud.com/ag-push/",
 	sendMetricInfo: true,
 	android: {
 		senderID : "517285908032",
 		variantID : "944ad613-5917-46e1-89ba-040c1006ee54",
 		variantSecret : "4260645d-203b-4a14-b7dc-debf2a162e9a"
-	},
-	success: eventSuccess,
-	error: eventError,
-	onNotification: onNotification
-};
+    },
+	ios: { 
+	    variantID: "f04fbf88-305b-4370-9006-d7019cce4991",
+	    variantSecret: "d8709c28-6969-4389-a4ac-705ecc903f99"
+	  }
+});
 
-push.registerPush(pushConfig);
+AeroGearPush.registerDevice({
+    extraOptions: {
+        categories: ["banana", "apple"]
+    },
+    onReceive: function(event) {
+        // Track Push Open
+        var pushId = event.payload["aerogear-push-id"];
+        AeroGearPush.trackPushOpenEvent(pushId);
+
+		items.push({ label: {text: event.alert}});
+		$.section.setItems(items);
+		$.main.open();
+    },
+    onTokenSuccess: function() {
+    	$.empty.open();
+    }
+});
 
 $.indicator.show();
 $.win.open();
