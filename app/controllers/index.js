a18597d7a709e68ce4acc01dc3a547788e8b1da1
Erik Jan de Wit <erikjan.dewit@gmail.com>
2016-04-15 22:17:29 +0200

    change with template

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,17 +1,17 @@
 var items = [];
 
 var AeroGearPush = require('AeroGearPush').init({
-	pushServerURL : "https://unifiedpush-edewit.rhcloud.com/ag-push/",
-	sendMetricInfo: true,
-	android: {
-		senderID : "517285908032",
-		variantID : "944ad613-5917-46e1-89ba-040c1006ee54",
-		variantSecret : "4260645d-203b-4a14-b7dc-debf2a162e9a"
+    pushServerURL: "<pushServerURL e.g http(s)//host:port/context >",
+    alias: "<alias e.g. a username or an email address optional>",
+    android: {
+      senderID: "<senderID e.g Google Project ID only for android>",
+      variantID: "<variantID e.g. 1234456-234320>",
+      variantSecret: "<variantSecret e.g. 1234456-234320>"
     },
-	ios: { 
-	    variantID: "f04fbf88-305b-4370-9006-d7019cce4991",
-	    variantSecret: "d8709c28-6969-4389-a4ac-705ecc903f99"
-	  }
+    ios: {
+      variantID: "<variantID e.g. 1234456-234320>",
+      variantSecret: "<variantSecret e.g. 1234456-234320>"
+    }
 });
 
 AeroGearPush.registerDevice({
