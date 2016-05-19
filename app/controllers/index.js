145fd9e00fb988565b5af164c84e6777c6d10fe3
Erik Jan de Wit <erikjan.dewit@gmail.com>
2016-05-19 11:06:29 +0200

    changed to use fh-js-sdk

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,35 +1,13 @@
 var items = [];
 
-var AeroGearPush = require('AeroGearPush').init({
-    pushServerURL: "<pushServerURL e.g http(s)//host:port/context >",
-    alias: "<alias e.g. a username or an email address optional>",
-    android: {
-      senderID: "<senderID e.g Google Project ID only for android>",
-      variantID: "<variantID e.g. 1234456-234320>",
-      variantSecret: "<variantSecret e.g. 1234456-234320>"
-    },
-    ios: {
-      variantID: "<variantID e.g. 1234456-234320>",
-      variantSecret: "<variantSecret e.g. 1234456-234320>"
-    }
-});
+var $fh = require('feedhenry-titanium');
 
-AeroGearPush.registerDevice({
-    extraOptions: {
-        categories: ["banana", "apple"]
-    },
-    onReceive: function(event) {
-        // Track Push Open
-        var pushId = event.payload["aerogear-push-id"];
-        AeroGearPush.trackPushOpenEvent(pushId);
-
-		items.push({ label: {text: event.alert}});
-		$.section.setItems(items);
-		$.main.open();
-    },
-    onTokenSuccess: function() {
-    	$.empty.open();
-    }
+$fh.push(function (event) {
+  items.push({ label: { text: event.alert } });
+  $.section.setItems(items);
+  $.main.open();
+}, function () {
+  $.empty.open();
 });
 
 $.indicator.show();
---
+++ app/lib/fhconfig.js
@@ -0,0 +1,17 @@
+module.exports = {
+	"host" : "",
+	"appid" : "",
+	"projectid" : "",
+	"appkey" : "",
+	"connectiontag" : "",
+	"pushServerURL" : "",
+	"android" : {
+		"senderID" : "",
+		"variantID" : "",
+		"variantSecret" : ""
+	},
+	"ios" : {
+		"variantID" : "",
+		"variantSecret" : ""
+	}
+}; 
